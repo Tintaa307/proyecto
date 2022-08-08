bf7d80cb97981e732569b30d5b4a989c5a4654b6
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-08-08 15:34:57 -0300

    trans

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react"
+import React, { useState, useEffect, useContext, useRef } from "react"
 import "./home.css"
 import { motion } from "framer-motion"
 import { useTypewriter } from "react-simple-typewriter"
